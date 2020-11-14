98d2e5aa7802d7ab21d76429da405c91709bd1b6
sundirrala <nisara.drahman@gmail.com>
2020-11-13 23:00:35 -0500

    enemy movement

---
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public Joystick joystick;
+    public float joystickHorizontalSensitivity;
+    public float joystickVerticalSensitivity;
+    public float horizontalForce;
+    public float verticalForce;
+    public bool isGrounded;
+    public bool isJumping;
+    public bool isCrouching;
+    public Transform spawnPoint;
+
+    private Rigidbody2D m_rigidBody2D;
+    private SpriteRenderer m_spriteRenderer;
+    private Animator m_animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rigidBody2D = GetComponent<Rigidbody2D>();
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
+        m_animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _Move();
+    }
+
+    void _Move()
+    {
+        if (isGrounded)
+        {
+            if(!isJumping && !isCrouching)
+            {
+                if (joystick.Horizontal > joystickHorizontalSensitivity)
+                {
+                    // move right
+                    m_rigidBody2D.AddForce(Vector2.right * horizontalForce * Time.deltaTime);
+                    m_spriteRenderer.flipX = false;
+                    m_animator.SetInteger("AnimState", (int)PlayerAnimationType.RUN);
+                }
+                else if (joystick.Horizontal < -joystickHorizontalSensitivity)
+                {
+                    // move left
+                    m_rigidBody2D.AddForce(Vector2.left * horizontalForce * Time.deltaTime);
+                    m_spriteRenderer.flipX = true;
+                    m_animator.SetInteger("AnimState", (int)PlayerAnimationType.RUN);
+                }
+                else
+                {
+                    m_animator.SetInteger("AnimState", (int)PlayerAnimationType.IDLE);
+                }
+            }
+
+
+
+            if ((joystick.Vertical > joystickVerticalSensitivity) && (!isJumping))
+            {
+                // jump
+                m_rigidBody2D.AddForce(Vector2.up * verticalForce);
+                m_animator.SetInteger("AnimState", (int) PlayerAnimationType.JUMP);
+                isJumping = true;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+
+        if ((joystick.Vertical < -joystickVerticalSensitivity) && (!isCrouching))
+        {
+            m_animator.SetInteger("AnimState", (int)PlayerAnimationType.CROUCH);
+            isJumping = true;
+        }
+        else
+        {
+            isJumping = false;
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Platforms"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Platforms"))
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // respawn
+        if (other.gameObject.CompareTag("DeathPlane"))
+        {
+            transform.position = spawnPoint.position;
+        }
+    }
+}
---
+++ Assets/_Scripts/opossumBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class opossumBehaviour : MonoBehaviour
+{
+    public float runForce;
+    public Rigidbody2D rigidbody2D;
+    public Transform lookAheadPoint;
+    public LayerMask collisionLayer;
+    public bool isGroundedAhead;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _LookAhead();
+        _Move();
+    }
+
+    private void _LookAhead()
+    {
+        isGroundedAhead = Physics2D.Linecast(transform.position, lookAheadPoint.position, collisionLayer);
+        Debug.DrawLine(transform.position, lookAheadPoint.position, Color.cyan);
+    }
+
+    private void _Move()
+    {
+        if (isGroundedAhead)
+        {
+            rigidbody2D.AddForce(Vector2.left * runForce * Time.deltaTime * transform.localScale.x);
+
+            rigidbody2D.velocity *= 0.90f;
+        }
+        else
+        {
+            transform.localScale = new Vector3(transform.localScale.x * -1.0f, transform.localScale.y, transform.localScale.z);
+        }
+
+    }
+
+
+
+}
